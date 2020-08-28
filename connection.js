2814e36e21be00ebb385320ec9a9c24895b9baa7
Jacob Plumb <jacobmaxplumb@gmail.com>
2020-08-27 21:51:01 -0400

    updated

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const connectionString = 'postgres://zbqujjlymnbkok:f573c5134aeeadc1c58dfea82c391a39b427c77518fe9a5dfc28406248702937@ec2-34-195-115-225.compute-1.amazonaws.com:5432/deqk0orch11qq';
+const connectionString = '<your postgre uri>';
 const pool = new Pool({
     connectionString: connectionString,
     ssl: connectionString.includes('localhost') ? false : {
